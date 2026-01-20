91251bdb9369956ee6f815f17631d15d2379a1a8
Bigeng Wang <bw2482@columbia.edu>
2026-01-20 14:12:50 -0800

    Updated the return type for add_entry for the circular buffer for dynamic allocation.

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -53,14 +53,15 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 * If the buffer was already full, overwrites the oldest entry and advances buffer->out_offs to the
 * new start location.
 * Any necessary locking must be handled by the caller
-* Any memory referenced in @param add_entry must be allocated by and/or must have a lifetime managed by the caller.
+* @return NULL or, if an existing entry at out_offs was repalced,
+*         the value of buffptr for the entry which was replaced (for use with dynamic memory allocation/free 
 */
-void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
+const char *aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
     /**
     * TODO: implement per description
     */
-	
+	struct aesd_buffer_entry *entry_backup = buffer->entry[buffer->in_offs];	
 	buffer->entry[buffer->in_offs] = *add_entry; // write or overwrites the oldest entry
 	// check if the current in_offs is the last one
 	if (buffer->in_offs == (AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED-1)) {
@@ -72,7 +73,9 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
 	}
 	if (buffer->full == true) { // Buffer already full, advances buffer->out_offs to the new start location
 		buffer->out_offs = buffer->in_offs;
+		return entry_backup; // return this entry to be replace for dynamic removal
 	}	
+	return NULL; // By default
 }
 
 /**
